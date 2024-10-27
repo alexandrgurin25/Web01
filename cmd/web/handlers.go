f361c509af132ae1ce6f9304bf068b7636f3787b
rollybin <rollybin@student.21-school.ru>
2024-10-27 17:29:46 +0300

    На стадии внедрения jwt

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -36,24 +36,54 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// Обработчик страницы логина.
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/login" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию template.ParseFiles() для чтения файла шаблона.
+	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// используя функцию http.Error() мы отправим пользователю
+	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	ts, err := template.ParseFiles("../../ui/html/login.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+}
+
 func writeInDb(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method != http.MethodPost {
-		// Если это не так, то вызывается метод w.WriteHeader() для возвращения статус-кода 405
-		// и вызывается метод w.Write() для возвращения тела-ответа с текстом "Метод запрещен".
-		// Затем мы завершаем работу функции вызвав "return", чтобы
-		// последующий код не выполнялся.
-		w.WriteHeader(405)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("GET-Метод запрещен!"))
 		return
 	}
 
-	_, err := db.Exec(
-		`INSERT INTO users (name) VALUES ($1)`,
-		r.FormValue("name"),
-	)
-
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Unable to execute the query", http.StatusInternalServerError)
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, "Имя не может быть пустым", http.StatusBadRequest)
+		return
 	}
+
+	_, err := db.Exec(`INSERT INTO users (name) VALUES ($1)`, name)
+	if err != nil {
+		log.Println("Ошибка при выполнении запроса:", err)
+		http.Error(w, "Unable to execute the query", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Данные успешно добавлены"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,27 +4,30 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
-    _ "github.com/lib/pq"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	// Подключение к базе данных
-	connStr := "user=postgres dbname=postgres sslmode=disable"
+	password := os.Getenv("DB_PASSWORD") // Получаем пароль из переменной окружения
+	connStr := "user=postgres password=" + password + " dbname=postgres sslmode=disable"
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
 	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
 	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/submit", 
-    func(w http.ResponseWriter, r *http.Request) {
-        writeInDb(w, r, db)
-    })
-
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/submit",
+		func(w http.ResponseWriter, r *http.Request) {
+			writeInDb(w, r, db)
+		})
 
 	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
 	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
